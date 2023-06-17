e62f8d3c87c3f76cd02866966d107c1c88aac33e
DenZhigalov <denzhigalov@contorra.com>
2023-06-17 20:33:26 +0700

    changed vimspector and lsp keybindings

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -8,39 +8,14 @@ local lsp = require('lsp-zero').preset({})
 
 
 
--- local cmp = require("cmp")
--- local cmp_select = {behavior = cmp.SelectBehavior.Select}
--- local cmp_mappings = lsp.defaults.cmp_mappings({
---     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
---     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
---     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
---     ['<C-Space>'] = cmp.mapping.complete(),
--- 
--- })
--- 
--- lsp.set_preferences({
---     sign_icons = { }
--- })
--- 
--- lsp.setup_nvim_cmp({
---     mapping = cmp_mappings
--- })
--- 
--- lsp.on_attach(function(client, bufnr)
---     local opts = {buffer = bufnr, remap = false}
--- 
---     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
---     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
---     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
---     vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.open_float() end, opts)
---     vim.keymap.set("n", "[d", function() vim.lsp.buf.goto_next() end, opts)
---     vim.keymap.set("n", "]d", function() vim.lsp.buf.goto_prev() end, opts)
---     vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
---     vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
---     vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
---     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
--- end)
-
+ local cmp = require("cmp")
+ local cmp_select = {behavior = cmp.SelectBehavior.Select}
+ -- local cmp_mappings = lsp.defaults.cmp_mappings({
+ --     ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+ --     ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+ --     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+ --     ['<C-Space>'] = cmp.mapping.complete(),
+ -- })
 
 
 lsp.ensure_installed({
@@ -54,12 +29,15 @@ lsp.setup_servers({
     'texlab',
 })
 
-lsp.on_attach(function(client, bufnr) 
-    lsp.default_keymaps({buffer = bufnr}) 
-    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', {buffer = true})
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = true })
+    vim.keymap.set({ 'n', 'x' }, '<C-l>', function()
+        vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+    end)
 end)
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-require('lspconfig').kotlin_language_server.setup{}
+require('lspconfig').kotlin_language_server.setup {}
 
 lsp.setup()
